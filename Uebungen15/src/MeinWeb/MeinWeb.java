5832090df07c01ccd3557b98aef57ed6cde914e7
Felix Gallmetzer <felix.gallmetzer@gmail.com>
2022-10-19 10:40:05 +0200

    finished MeinWeb

--- Uebungen15/src/MeinWeb/MeinWeb.java
+++ Uebungen15/src/MeinWeb/MeinWeb.java
@@ -6,8 +6,14 @@ import javax.swing.event.HyperlinkListener;
 
 import java.util.*;
 import java.awt.*;
+import java.awt.RenderingHints.Key;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 
 public class MeinWeb extends JFrame
@@ -27,22 +33,43 @@ public class MeinWeb extends JFrame
 	
 	public MeinWeb() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		int framewidth = 700, frameheight = 800;
 		Dimension Screen = Toolkit.getDefaultToolkit().getScreenSize();
-		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setBounds(Screen.width/2 - framewidth, (int) (Screen.height - 1.2*frameheight), framewidth, frameheight);
+//		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setTitle("Mein Web");
+		this.setResizable(false);
 		
 		this.getContentPane().setLayout(null);
 		
 		adresse = new JLabel();
 		adresse.setBounds(10, 0, 60, 40);
 		adresse.setText("Adresse:");
+		adresse.setDisplayedMnemonic('S');
+		adresse.addFocusListener(
+				new FocusListener() {
+
+					public void focusGained(FocusEvent e) {
+						// TODO Auto-generated method stub
+						searchBar.selectAll();
+					}
+
+					public void focusLost(FocusEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+				});
 		this.getContentPane().add(adresse);
 		
 		searchBar = new JTextField();
-		searchBar.setBounds(70, 0, Screen.width-150, 40);
+		adresse.setLabelFor(searchBar);
+		searchBar.setBounds(70, 0, framewidth-150, 40);
+		EingabeListener el = new EingabeListener();
+		searchBar.addKeyListener(el);
 		this.getContentPane().add(searchBar);
-		
 		bestaetigen = new JButton();
-		bestaetigen.setBounds(Screen.width-70, 0, 60, 40);
+		bestaetigen.setBounds(framewidth-70, 0, 60, 40);
 		bestaetigen.setText("➔");
 //		bestaetigen.setFont(new Font("Arial", Font.PLAIN, 20));
 		bestaetigen.addActionListener(
@@ -56,25 +83,25 @@ public class MeinWeb extends JFrame
 		this.getContentPane().add(bestaetigen);
 		
 		website = new JEditorPane();
-		website.setEditable(true);
+		website.setEditable(false);
 		website.setEditorKit(JEditorPane.createEditorKitForContentType("text/html"));
 		website.addHyperlinkListener(new HyperlinkListener() {
 			
 			public void hyperlinkUpdate(HyperlinkEvent e) {
-				searchBar.setText(e.getURL().toString());
-				loadWebsite();
+				if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+					searchBar.setText(e.getURL().toString());
+					loadWebsite();
+				}
 			}
 		});
 		leiste = new JScrollPane(website);
-		leiste.setBounds(0,40,Screen.width,950);
+		leiste.setBounds(0,40,framewidth-6,frameheight-76);
 		this.getContentPane().add(leiste);
 	}
 	
 	public void loadWebsite() {
 		try {
-			if (searchBar.getText().substring(0,8) == "https://") {
-				website.setPage(searchBar.getText());
-			} else if(searchBar.getText().substring(0,7) == "http://") {
+			if (searchBar.getText().length() < 7 || searchBar.getText().substring(0,8).equals("https://") || searchBar.getText().substring(0,7).equals("http://")) {
 				website.setPage(searchBar.getText());
 			} else {
 				searchBar.setText("https://" + searchBar.getText());
@@ -82,9 +109,23 @@ public class MeinWeb extends JFrame
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(this, "Kann dem Hyperlink nicht folgen", "Fehler", JOptionPane.ERROR_MESSAGE);
 		}
 		this.setTitle("Mein Web - " + searchBar.getText());
 	}
+	
+	
+	public class EingabeListener extends KeyAdapter {
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+				loadWebsite();
+			}
+		}
+	}
 
 }
+
+
